34db1a19f774afd828d8d8e40b0e6bbea08d7625
Adam Liewehr <al8425b@american.edu>
2025-09-25 13:23:52 -0400

    save
    have a working game, just need to refine it!

--- 05-external-libraries/script.js
+++ 05-external-libraries/script.js
@@ -1,78 +1,159 @@
 let dots = [];
+let enemies = [];
 
 function setup() {
-  // put setup code here
+    // put setup code here
 
-  createCanvas(800, 800);
+    createCanvas(800, 800);
+    frameRate(240);
 
-  for (let i = 0; i < 100; i++) {
-    let dot = new Dot(width / 2, height / 2, i);
-    dots.push(dot);
+    for (let i = 0; i < 1; i++) {
+        let dot = new Dot(width / 2, height / 2, i);
+        dots.push(dot);
 
-  }
+    }
+
+    for (let i = 0; i < 2; i++) {
+        let enemy = new Enemy(Math.random()*width, Math.random()*height);
+        enemies.push(enemy);
+
+    }
 
 
 
+}
+
+let enemyImg;
+
+function preload() {
+  enemyImg = loadImage('enemy.png'); 
 }
 
 function draw() {
-  // put drawing code here
-  background('white');
+    // put drawing code here
+    background('white');
 
-  fill(mouseX / width * 360, 100, 100, 100);
-  stroke('black');
-  strokeWeight(10);
-  ellipse(mouseX, mouseY, mouseX, mouseY);
+    // fill(mouseX / width * 360, 100, 100, 100);
+    // stroke('black');
+    // strokeWeight(10);
+    // ellipse(mouseX, mouseY, mouseX, mouseY);
 
-  for (let dot of dots) {
-    dot.draw();
+    for (let dot of dots) {
+        dot.draw();
+        for (let enemy of enemies) {
+            if (dist(dot.x, dot.y, enemy.x, enemy.y)<30) {
+                ellipse(width/2, height/2, 200, 200);
+                
+            }
+        }
 
-  }
+    }
+
+    for (let enemy of enemies) {
+        enemy.draw();
+    }
+    
 
 
 
 
 }
+
+function mousePressed() {
+    for (let dot of dots) {
+        dot.clicked();
+    }
+}
+
+
 
 
 class Dot {
 
-  constructor(x, y, index) {
-    this.x = x;
-    this.y = y;
-    this.index = index;
-    this.hue = Math.random() * 360;
-    this.radius = 100 + dots.length - this.index * 2;
-
-    this.velocityX = random(-5, 5);
-    this.velocityY = random(-5, 5);
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.hue = Math.random() * 360;
+        this.radius = 25;
+        this.isVisible = true;
 
 
+        this.dx = random(-5, 5);
+        this.dy = random(-5, 5);
 
-  }
-
-  draw() {
-    // this.x += (-0.5 + Math.random()) * 10;
-    // this.y += (-0.5 + Math.random()) * 10;
-
-    this.x += this.velocityX;
-    this.y += this.velocityY;
-
-    if (this.x > width || this.x < 0) {
-      this.velocityX *= -1;
     }
 
-    if (this.y > height || this.y < 0) {
-      this.velocityY *= -1;
+    draw() {
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+
+        // Only draw the dot if it's visible
+        if (this.isVisible) {
+            fill(108, 21, 12);
+            ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+        }
+
+        
+        // learned this through CMU Academy when I taught last year
+        let directionalVector = [mouseX - this.x, mouseY - this.y];
+        let magnitude = sqrt(directionalVector[0]**2+directionalVector[1]**2);
+        let normalizedVector = [directionalVector[0]/magnitude, directionalVector[1]/magnitude];
+
+        this.dx = normalizedVector[0]*2;
+        this.dy = normalizedVector[1]*2;
+
+    }
+
+    clicked() {
+
+        let distance = dist(mouseX, mouseY, this.x, this.y);
+        if (distance < this.radius) {
+            
+            this.isVisible = false;
+        }
+
+    }
+
+}
+
+class Enemy {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 30;
+        this.img = enemyImg;
+
+        this.dx = 0;
+        this.dy = 0;
+
+    }
+
+    draw() {
+        
+
+        this.x += this.dx;
+        this.y += this.dy;
+
+
+
+        imageMode(CENTER);
+        image(this.img, this.x, this.y, 50, 50);
+
+        
+        // learned this through CMU Academy when I taught last year
+        let directionalVector = [mouseX - this.x, mouseY - this.y];
+        let magnitude = sqrt(directionalVector[0]**2+directionalVector[1]**2);
+        let normalizedVector = [directionalVector[0]/magnitude, directionalVector[1]/magnitude];
+
+        this.dx = normalizedVector[0];
+        this.dy = normalizedVector[1];
+
     }
 
 
 
-    //this.x += Map(Math.random(), 0, 1, -1, 1);
-
-
-    fill(255, 255, 255, fill.hue);
-    ellipse(this.x, this.y, this.radius, this.radius);
-  }
 
 }
