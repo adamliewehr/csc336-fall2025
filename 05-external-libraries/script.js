b6575454e4162d10d0d13cb99c269b41a4c88f45
Adam Liewehr <al8425b@american.edu>
2025-09-25 14:49:31 -0400

    game works better
    nice!

--- 05-external-libraries/script.js
+++ 05-external-libraries/script.js
@@ -13,8 +13,8 @@ function setup() {
 
     }
 
-    for (let i = 0; i < 2; i++) {
-        let enemy = new Enemy(Math.random()*width, Math.random()*height);
+    for (let i = 0; i < 5; i++) {
+        let enemy = new Enemy(Math.random() * width, Math.random() * height);
         enemies.push(enemy);
 
     }
@@ -24,35 +24,72 @@ function setup() {
 }
 
 let enemyImg;
+let pizzaImg;
+let playButtonImg;
+let explosionImg;
 
 function preload() {
-  enemyImg = loadImage('enemy.png'); 
+    enemyImg = loadImage('enemy.png');
+    pizzaImg = loadImage('pizza.png');
+    playButtonImg = loadImage('playButton.png');
+    explosionImg = loadImage('explosion.gif');
+
 }
+
+let lostYet = false;
+let clickedPlay = false;
+
+let saveX = 0;
+let saveY = 0;
 
 function draw() {
     // put drawing code here
     background('white');
+
+    if (!clickedPlay) {
+
+        imageMode(CENTER);
+        image(playButtonImg, width / 2, height / 2, 100, 100);
+
+    }
+
+
+
 
     // fill(mouseX / width * 360, 100, 100, 100);
     // stroke('black');
     // strokeWeight(10);
     // ellipse(mouseX, mouseY, mouseX, mouseY);
 
-    for (let dot of dots) {
-        dot.draw();
-        for (let enemy of enemies) {
-            if (dist(dot.x, dot.y, enemy.x, enemy.y)<30) {
-                ellipse(width/2, height/2, 200, 200);
-                
+    
+
+    if (!lostYet && clickedPlay) {
+        for (let dot of dots) {
+            dot.draw();
+            for (let enemy of enemies) {
+                enemy.draw(dot.x, dot.y);
+                if (dist(dot.x, dot.y, enemy.x, enemy.y) < 30) {
+                    saveX = dot.x;
+                    saveY = dot.y;
+
+                    lostYet = true;
+
+                }
             }
+
         }
 
+        // for (let enemy of enemies) {
+            
+        // }
+
     }
 
-    for (let enemy of enemies) {
-        enemy.draw();
+    if (lostYet) {
+        imageMode(CENTER);
+        image(explosionImg, saveX, saveY, 300, 300);
     }
-    
+
 
 
 
@@ -60,8 +97,13 @@ function draw() {
 }
 
 function mousePressed() {
-    for (let dot of dots) {
-        dot.clicked();
+    // for (let dot of dots) {
+    //     dot.clicked();
+    // }
+
+    if (dist(mouseX, mouseY, width / 2, height / 2) < 100) {
+        clickedPlay = true;
+
     }
 }
 
@@ -78,6 +120,8 @@ class Dot {
         this.radius = 25;
         this.isVisible = true;
 
+        this.img = pizzaImg;
+
 
         this.dx = random(-5, 5);
         this.dy = random(-5, 5);
@@ -91,36 +135,41 @@ class Dot {
 
 
         // Only draw the dot if it's visible
-        if (this.isVisible) {
-            fill(108, 21, 12);
-            ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
-        }
+        // if (this.isVisible) {
+        //     fill(108, 21, 12);
+        //     ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+        // }
 
-        
+        imageMode(CENTER);
+        image(this.img, this.x, this.y, 30, 30);
+
+
+
+
         // learned this through CMU Academy when I taught last year
         let directionalVector = [mouseX - this.x, mouseY - this.y];
-        let magnitude = sqrt(directionalVector[0]**2+directionalVector[1]**2);
-        let normalizedVector = [directionalVector[0]/magnitude, directionalVector[1]/magnitude];
+        let magnitude = sqrt(directionalVector[0] ** 2 + directionalVector[1] ** 2);
+        let normalizedVector = [directionalVector[0] / magnitude, directionalVector[1] / magnitude];
 
-        this.dx = normalizedVector[0]*2;
-        this.dy = normalizedVector[1]*2;
-
-    }
-
-    clicked() {
-
-        let distance = dist(mouseX, mouseY, this.x, this.y);
-        if (distance < this.radius) {
-            
-            this.isVisible = false;
-        }
+        this.dx = normalizedVector[0] * 2;
+        this.dy = normalizedVector[1] * 2;
 
     }
+
+    // clicked() {
+
+    //     let distance = dist(mouseX, mouseY, this.x, this.y);
+    //     if (distance < this.radius) {
+
+    //         this.isVisible = false;
+    //     }
+
+    // }
 
 }
 
 class Enemy {
-    constructor (x, y) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.radius = 30;
@@ -131,8 +180,8 @@ class Enemy {
 
     }
 
-    draw() {
-        
+    draw(toX, toY) {
+
 
         this.x += this.dx;
         this.y += this.dy;
@@ -142,11 +191,11 @@ class Enemy {
         imageMode(CENTER);
         image(this.img, this.x, this.y, 50, 50);
 
-        
+
         // learned this through CMU Academy when I taught last year
-        let directionalVector = [mouseX - this.x, mouseY - this.y];
-        let magnitude = sqrt(directionalVector[0]**2+directionalVector[1]**2);
-        let normalizedVector = [directionalVector[0]/magnitude, directionalVector[1]/magnitude];
+        let directionalVector = [toX - this.x, toY - this.y];
+        let magnitude = sqrt(directionalVector[0] ** 2 + directionalVector[1] ** 2);
+        let normalizedVector = [directionalVector[0] / magnitude, directionalVector[1] / magnitude];
 
         this.dx = normalizedVector[0];
         this.dy = normalizedVector[1];
