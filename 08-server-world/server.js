05e8bd7267506ce0f5aa02095d005e49e5b66080
Adam Liewehr <al8425b@american.edu>
2025-10-28 17:44:33 -0400

    cors

--- 08-server-world/server.js
+++ 08-server-world/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import fs from "fs";
+import cors from "cors";
 
 // creating an instance of the express server
 const app = express();
 
 app.use(express.static("./public"));
 app.use(express.json());
+app.use(cors());
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
 
